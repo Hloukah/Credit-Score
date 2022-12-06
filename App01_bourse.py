28f940d481f618c97697509f6cba135e76d7be1a
Hloukah <98806109+Hloukah@users.noreply.github.com>
2022-12-06 10:45:02 +0100

    Update App01_bourse.py

--- App01_bourse.py
+++ App01_bourse.py
@@ -8,7 +8,7 @@ st.write("""
 Shown are the stock closing price and volume of Google
 """)
 
-'''#Define stock
+"""#Define stock
 tickerSymbol = "AAPL"
 
 #Get data
@@ -17,4 +17,4 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2004-1-1',end='2022-9-2')
 
 st.line_chart(tickerDf.Close)
-st.line_chart(tickerDf.Volume)'''
+st.line_chart(tickerDf.Volume)"""
