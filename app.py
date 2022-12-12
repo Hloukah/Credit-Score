88f8f5549f5b10c4be4c588f887e7326ea55008f
Hloukah <98806109+Hloukah@users.noreply.github.com>
2022-12-12 06:17:13 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 
-from sklearn.cluster import KMeans
+#from sklearn.cluster import KMeans
 from zipfile import ZipFile
 import plotly.express as px
 import shap
@@ -13,48 +13,47 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
 import streamlit as st
-import numpy as np
+#import numpy as np
 import pickle
 
 # loading the saved model
-loaded_model = pickle.load(
-    open('C:/Users/hlouk/OpenClassroom/P7/LGBMClassifier.pkl', 'rb')
+loaded_model = pickle.load(open('C:/Users/hlouk/OpenClassroom/P7/LGBMClassifier.pkl', 'rb'))
 
 plt.style.use('fivethirtyeight')
 # sns.set_style(''darkgrid')
 
 
 def main():
-    ''' @st.cache
-     def load_data():
-         z = ZipFile("data/default_risk.zip")
-         data = pd.read_csv(z.open('default_risk.csv'),
+      @st.cache
+      def load_data():
+         #z = ZipFile("data/default_risk.zip")
+         data = pd.read_csv(open('application_train.csv'),
                             index_col='SK_ID_CURR', encoding ='utf-8')
 
-         z = ZipFile("data/X_sample.zip")
+         z = ZipFile("X_sample.zip")
          sample = pd.read_csv(z.open('X_sample.csv'),
                               index_col='SK_ID_CURR', encoding ='utf-8')
 
-         description = pd.read_csv("data/features_description.csv",
+         description = pd.read_csv("HomeCredit_columns_description.csv",
                                    usecols=['Row', 'Description'], index_col=0, encoding= 'unicode_escape')
 
          target = data.iloc[:, -1:]
 
-         return data, sample, target, description'''
+         return data, sample, target, description
 
-    def load_model():
+      def load_model():
         '''loading the trained model'''
         pickle_in = open('LGBMClassifier.pkl', 'rb')
         clf = pickle.load(pickle_in)
         return clf
 
-    @st.cache(allow_output_mutation=True)
-    def load_knn(sample):
-        knn = knn_training(sample)
-        return knn
+     #@st.cache(allow_output_mutation=True)
+    #def load_knn(sample):
+     #   knn = knn_training(sample)
+      #  return knn'''
 
-    @st.cache    
-    def load_infos_gen(data):
+      @st.cache    
+      def load_infos_gen(data):
         lst_infos = [data.shape[0],
                      round(data["AMT_INCOME_TOTAL"].mean(), 2),
                      round(data["AMT_CREDIT"].mean(), 2)]
@@ -67,98 +66,98 @@ def main():
 
         return nb_credits, rev_moy, credits_moy, targets
 
-    def identite_client(data, id):
+      def identite_client(data, id):
         data_client = data[data.index == int(id)]
         return data_client
 
-    @st.cache
-    def load_age_population(data):
+      @st.cache
+      def load_age_population(data):
         data_age = round((data["DAYS_BIRTH"]/365), 2)
         return data_age
 
-    @st.cache
-    def load_income_population(sample):
+      @st.cache
+      def load_income_population(sample):
         df_income = pd.DataFrame(sample["AMT_INCOME_TOTAL"])
         df_income = df_income.loc[df_income['AMT_INCOME_TOTAL'] < 200000, :]
         return df_income
 
-    @st.cache
-    def load_prediction(sample, id, clf):
+      @st.cache
+      def load_prediction(sample, id, clf):
         X = sample.iloc[:, :-1]
         score = clf.predict_proba(X[X.index == int(id)])[:, 1]
         return score
 
-    @st.cache
-    def load_kmeans(sample, id, mdl):
+      ''' @st.cache
+      def load_kmeans(sample, id, mdl):
         index = sample[sample.index == int(id)].index.values
         index = index[0]
         data_client = pd.DataFrame(sample.loc[sample.index, :])
         df_neighbors = pd.DataFrame(knn.fit_predict(data_client), index=data_client.index)
         df_neighbors = pd.concat([df_neighbors, data], axis=1)
-        return df_neighbors.iloc[:, 1:].sample(10)
+        return df_neighbors.iloc[:, 1:].sample(10)'''
 
-        @st.cache
+      '''@st.cache
         def knn_training(sample):
             knn = KMeans(n_clusters=2).fit(sample)
-            return knn
+            return knn'''
 
 
     # Loading data……
-    data, sample, target, description = load_data()
-    id_client = sample.index.values
-    clf = load_model()
+      data, sample, target, description = load_data()
+      id_client = sample.index.values
+      clf = load_model()
 
     #######################################
     # SIDEBAR
     #######################################
 
     # Title display
-    html_temp = """
+      html_temp = """
     <div style="background-color: tomato; padding:10px; border-radius:10px">
     <h1 style="color: white; text-align:center">Dashboard Scoring Credit</h1>
     </div>
     <p style="font-size: 20px; font-weight: bold; text-align:center">Credit decision support…</p>
     """
-    st.markdown(html_temp, unsafe_allow_html=True)
+      st.markdown(html_temp, unsafe_allow_html=True)
 
     # Customer ID selection
-    st.sidebar.header("**General Info**")
+      st.sidebar.header("**General Info**")
 
     # Loading selectbox
-    chk_id = st.sidebar.selectbox("Client ID", id_client)
+      chk_id = st.sidebar.selectbox("Client ID", id_client)
 
     # Loading general info
-    nb_credits, rev_moy, credits_moy, targets = load_infos_gen(data)
+      nb_credits, rev_moy, credits_moy, targets = load_infos_gen(data)
 
     ### Display of information in the sidebar ###
     # Number of loans in the sample
-    st.sidebar.markdown("<u>Number of loans in the sample :</u>", unsafe_allow_html=True)
-    st.sidebar.text(nb_credits)
+      st.sidebar.markdown("<u>Number of loans in the sample :</u>", unsafe_allow_html=True)
+      st.sidebar.text(nb_credits)
 
     # Average income
-    st.sidebar.markdown("<u>Average income (USD) :</u>", unsafe_allow_html=True)
-    st.sidebar.text(rev_moy)
+      st.sidebar.markdown("<u>Average income (USD) :</u>", unsafe_allow_html=True)
+      st.sidebar.text(rev_moy)
 
     # AMT CREDIT
-    st.sidebar.markdown("<u>Average loan amount (USD) :</u>", unsafe_allow_html=True)
-    st.sidebar.text(credits_moy)
+      st.sidebar.markdown("<u>Average loan amount (USD) :</u>", unsafe_allow_html=True)
+      st.sidebar.text(credits_moy)
 
     # PieChart
-    #st.sidebar.markdown("<u>......</u>", unsafe_allow_html=True)
-    fig, ax = plt.subplots(figsize=(5, 5))
-    plt.pie(targets, explode=[0, 0.1], labels=['No default', 'Default'], autopct='%1.1f%%', startangle=90)
-    st.sidebar.pyplot(fig)
+     #st.sidebar.markdown("<u>......</u>", unsafe_allow_html=True)
+      fig, ax = plt.subplots(figsize=(5, 5))
+      plt.pie(targets, explode=[0, 0.1], labels=['No default', 'Default'], autopct='%1.1f%%', startangle=90)
+      st.sidebar.pyplot(fig)
 
     #######################################
     # HOME PAGE - MAIN CONTENT
     #######################################
     # Display Customer ID from Sidebar
-    st.write("Customer ID selection :", chk_id)
+      st.write("Customer ID selection :", chk_id)
 
     # Customer information display : Customer Gender, Age, Family status, Children, …
-    st.header("**Customer information display**")
+      st.header("**Customer information display**")
 
-    if st.checkbox("Show customer information ?"):
+      if st.checkbox("Show customer information ?"):
 
         infos_client = identite_client(data, chk_id)
         st.write("**Gender : **", infos_client["CODE_GENDER"].values[0])
@@ -208,13 +207,13 @@ def main():
 
         st.plotly_chart(fig)
 
-    else:
+      else:
         st.markdown("<i>…</i>", unsafe_allow_html=True)
 
     # Customer solvability display
-    st.header("**Customer file analysis**")
-    prediction = load_prediction(sample, chk_id, clf)
-    st.write("**Default probability : **{:.0f} %".format(round(float(prediction)*100, 2)))
+      st.header("**Customer file analysis**")
+      prediction = load_prediction(sample, chk_id, clf)
+      st.write("**Default probability : **{:.0f} %".format(round(float(prediction)*100, 2)))
 
     # Compute decision according to the best threshold
     # if prediction <= xx :
@@ -224,11 +223,11 @@ def main():
 
     #st.write("**Decision** *(with threshold xx%)* **: **", decision, unsafe_allow_html=True)
 
-    st.markdown("<u>Customer Data :</u>", unsafe_allow_html=True)
-    st.write(identite_client(data, chk_id))
+      st.markdown("<u>Customer Data :</u>", unsafe_allow_html=True)
+      st.write(identite_client(data, chk_id))
 
     # Feature importance / description
-    if st.checkbox("Customer ID {:.0f} feature importance ?".format(chk_id)):
+      if st.checkbox("Customer ID {:.0f} feature importance ?".format(chk_id)):
         shap.initjs()
         X = sample.iloc[:, :-1]
         X = X[X.index == chk_id]
@@ -245,24 +244,24 @@ def main():
             feature = st.selectbox('Feature checklist…', list_features)
             st.table(description.loc[description.index == feature][:1])
 
-    else:
+      else:
         st.markdown("<i>…</i>", unsafe_allow_html=True)
 
 
-    # Similar customer files display
-    chk_voisins = st.checkbox("Show similar customer files ?")
+      '''  # Similar customer files display
+     chk_voisins = st.checkbox("Show similar customer files ?")
 
-    if chk_voisins:
+     if chk_voisins:
         knn = load_knn(sample)
         st.markdown("<u>List of the 10 files closest to this Customer :</u>", unsafe_allow_html=True)
-        st.dataframe(load_kmeans(sample, chk_id, knn))
+        st.dataframe(load_kmeans(sample, chk_id))
         st.markdown("<i>Target 1 = Customer with default</i>", unsafe_allow_html=True)
-    else:
+     else:
         st.markdown("<i>…</i>", unsafe_allow_html=True)
 
-    st.markdown('***')
-    st.markdown("Thanks for going through this Web App with me! I'd love feedback on this, so if you want to reach out you can find me on [twitter] (https://twitter.com/nalron_) or my [website](https://nalron.com/). *Code from [Github](https://github.com/nalron/project_credit_scoring_model)* ❤️")
-
+     st.markdown('***')
+     st.markdown("Thanks for going through this Web App with me! I'd love feedback on this, so if you want to reach out you can find me on [twitter] (https://twitter.com/nalron_) or my [website](https://nalron.com/). *Code from [Github](https://github.com/nalron/project_credit_scoring_model)* ❤️")
+'''
 
 if __name__ == '__main__':
     main()
